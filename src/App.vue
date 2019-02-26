46d48b4274155b2db0aa2c195ba07977ecddc477
xia0bs <719398840@qq.com>
2019-02-27 00:51:30 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,9 @@
             :key="index"
             :class="{'active':index%2 !== 1}"
             :style="{backgroundImage: 'url(' + item.attributes['thumbnail-url-base'] + '?w=248&h=248' + ')', backgroundRepeat:'no-repeat', backgroundPosition:'center center', backgroundSize:'cover'}">
+            <div class="badge">
+                <span>{{item.attributes.platforms[0]}}</span>
+            </div>
             <div class="cover"></div>
             <p>{{item.attributes.name}}</p>
         </li>
@@ -61,8 +64,13 @@
   color: #2c3e50;
   max-width: 100%;
   height: 160px;
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  -ms-user-select: none;
+  -khtml-user-select: none;
   user-select: none;
-    padding: 0 20px 0;
+  padding: 0 15px 0;
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
 }
 *{
   padding: 0;
@@ -95,6 +103,31 @@
   overflow: hidden;
   text-overflow: ellipsis;
   color: #fff;
+}
+.badge{
+    content:"";
+    display:block;
+    height:0;
+    border-width:0px 15px 15px;
+    border-style:none solid solid;
+    border-color:transparent transparent #ffe312;
+    position:absolute;
+    -moz-transform:rotate(-45deg);/*FF浏览器*/
+    -webkit-transform:rotate(-45deg);/*chrome浏览器*/
+    -o-transform:rotate(-45deg);/*oprea浏览器*/
+    -ms-transform:rotate(-45deg);/*IE浏览器*/
+    left: -18px;
+    top: 17px;
+    width: 56px;
+}
+.badge span{
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    font-size: 12px;
+    color: #472400;
 }
 .cover{
   position: absolute;
